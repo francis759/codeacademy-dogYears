308bcaa99a1cb0ee0194d5b37eef39ad7db6bbc0
Francis Dowuona <82451457+francis759@users.noreply.github.com>
2021-07-18 12:41:43 +0000

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+const myAge = 28;
+let earlyYears = 2;
+earlyYears *= 10.5;
+let laterYears = myAge - 2;
+laterYears *= 4;
+var myAgeInDogYears = earlyYears+laterYears;
+const myName = 'Francis';
+'Grace Hopper'.toLowerCase();
+console.log('My name is '+ myName+ '. I am '+ myAge+' years old in human years which is '+myAgeInDogYears+' years old in dog years')
